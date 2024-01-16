0f718db4abb744901e2f6dda823ff178a2d272ac
SharanS6 <sharanrajan6@gmail.com>
2024-01-16 21:05:02 +0530

    updated education

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-var navbarSlide=document.getElementById("navbar-slide")
+var navbarSlide = document.getElementById("navbar-slide")
 
-var menubar=document.getElementById("menuBar")
+var menubar = document.getElementById("menuBar")
 
-var close=document.querySelector(".closeIcon")
+var close = document.querySelector(".closeIcon")
 
 
-menubar.addEventListener("click",function(){
-    navbarSlide.style.right="0%"
-    
+menubar.addEventListener("click", function () {
+    navbarSlide.style.right = "0%"
+
 })
 
 
 
-close.addEventListener("click",function(){
-    navbarSlide.style.right="-50%"
+close.addEventListener("click", function () {
+    navbarSlide.style.right = "-50%"
 })
 
 
-  // <!-- typed js effect starts -->
+// scrollEffect fadein and reveal
+window.addEventListener("scroll", function () {
+
+    //get all reveal class
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        // windowheight
+        var windowheight = this.window.innerHeight;
+        //element height
+        var revealtop = reveals[i].getBoundingClientRect().top
+        var revealpoint = 10;
+       //if windowheight less than element add class active else remove active class style
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active")
+        }
+        else {
+            reveals[i].classList.remove("active")
+        }
+    }
+}
+)
+// <!-- typed js effect starts -->
 var typed = new Typed(".autotype", {
     strings: ["Frontend Development", "Backend Development", "Web Designing", "Web Development"],
-    
+
     typeSpeed: 200,
     backSpeed: 100,
     backDelay: 500,
